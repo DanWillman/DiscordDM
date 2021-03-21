51899718af717ee5ab68d563b9ef1b72815f36f4
Dan Willman <github@danwillman.com>
2021-03-21 17:39:06 -0500

    add sln/proj structure

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DiscordDM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(TimeSpan.FromHours(1));
+        }
+    }
+}
